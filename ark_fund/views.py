426b2128693d9148a8b436c77bc534c52c3f7fbd
Shyam <s4shyam95@gmail.com>
2018-03-31 22:06:25 -0400

    Ankush

--- ark_fund/views.py
+++ ark_fund/views.py
@@ -19,11 +19,11 @@ def encode(key, clear):
         key_c = key[i % len(key)]
         enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
         enc.append(enc_c)
-    return base64.urlsafe_b64encode(bytes("".join(enc), 'ascii'))
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
 
 def decode(key, enc):
     dec = []
-    enc = base64.urlsafe_b64decode(enc)
+    enc = base64.urlsafe_b64decode(enc).decode()
     for i in range(len(enc)):
         key_c = key[i % len(key)]
         dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
